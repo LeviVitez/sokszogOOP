40eede3912394cc708bc53197dc70ea9a1f3e7ec
Bóta László Levente <bmwyolo555@gmail.com>
2022-10-04 15:22:06 +0200

    asdf

--- src/hu/petrik/sokszogoop/Haromszog.java
+++ src/hu/petrik/sokszogoop/Haromszog.java
@@ -1,14 +1,29 @@
 package hu.petrik.sokszogoop;
 
-public class Haromszog extends Sokszog{
+public class Haromszog extends Sokszog {
     private double b;
     private double c;
+
+    public Haromszog() {
+        super(getVeletlenOldal());
+        this.b = getVeletlenOldal();
+        this.c = getVeletlenOldal();
+        while (!isSzerkesztheto()){
+            super.setA(getVeletlenOldal());
+            this.b=getVeletlenOldal();
+            this.c=getVeletlenOldal();
+        }
+    }
+
+    private static double getVeletlenOldal() {
+        return Math.random() * 10 + 5;
+    }
 
     public Haromszog(double a, double b, double c) {
         super(a);
         this.b = b;
         this.c = c;
-        if (!this.isSzerkesztheto()){
+        if (!this.isSzerkesztheto()) {
             throw new IllegalArgumentException("A háromszög nem szerkeszthető");
         }
     }
@@ -23,14 +38,14 @@ public class Haromszog extends Sokszog{
 
     public void setB(double b) {
         this.b = b;
-        if (!this.isSzerkesztheto()){
+        if (!this.isSzerkesztheto()) {
             throw new IllegalArgumentException("A háromszög nem szerkeszthető");
         }
     }
 
     public void setC(double c) {
         this.c = c;
-        if (!this.isSzerkesztheto()){
+        if (!this.isSzerkesztheto()) {
             throw new IllegalArgumentException("A háromszög nem szerkeszthető");
         }
     }
@@ -38,37 +53,37 @@ public class Haromszog extends Sokszog{
     @Override
     public void setA(double a) {
         super.setA(a);
-        if (!this.isSzerkesztheto()){
+        if (!this.isSzerkesztheto()) {
             throw new IllegalArgumentException("A háromszög nem szerkeszthető");
         }
     }
 
-    private boolean isSzerkesztheto(){
-        boolean szerkesztheto= true;
-        if ((this.getA()+this.b)<=this.getC() ||
-                (this.getA()+this.getC())<=this.getB() ||
-                (this.getB()+this.getC())<= this.getC()){
-            szerkesztheto=false;
+    private boolean isSzerkesztheto() {
+        boolean szerkesztheto = true;
+        if ((this.getA() + this.b) <= this.getC() ||
+                (this.getA() + this.getC()) <= this.getB() ||
+                (this.getB() + this.getC()) <= this.getA()) {
+            szerkesztheto = false;
         }
 
         return szerkesztheto;
     }
 
-    public  double getKerulet(){
-    return this.getA()+this.getB()+this.getC();
+    public double getKerulet() {
+        return this.getA() + this.getB() + this.getC();
     }
 
-    private double getS(){
-        return getKerulet()/2;
+    private double getS() {
+        return getKerulet() / 2;
     }
 
-    public double getTerulet(){
-        return Math.sqrt(this.getS()*(this.getS()-this.getA())*(this.getS()-this.getB())*(this.getS()-this.getC()));
+    public double getTerulet() {
+        return Math.sqrt(this.getS() * (this.getS() - this.getA()) * (this.getS() - this.getB()) * (this.getS() - this.getC()));
     }
 
     @Override
     public String toString() {
-        //TODO: to string hozzáadása
-        return "";
+        return String.format("háromszög: a = %f - b = %f - c = %f - K = %f - T = %f",
+                this.getA(), this.getB(), this.getC(), this.getKerulet(), this.getTerulet());
     }
 }
